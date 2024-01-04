9ffce4d829949986d60055b9e531d7f30911639b
Andrii Balan <balan.andrey.93@gmail.com>
2024-01-04 10:06:31 +0200

    Update script.js
    timer date update

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
     modal('[data-modal]', '.modal', modalTimerId);
-    timer('.timer', '2023-12-31');
+    timer('.timer', '2024-12-31');
     cards();
     calc();
     forms('form', modalTimerId);
